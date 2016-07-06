a788a2d81deaa25af33ac6cd03b215c2ad53fe16
EC2 Default User <ec2-user@ip-172-31-22-93.ap-southeast-1.compute.internal>
2016-07-06 16:20:41 +0000

    Deleted forms.py and views.py which was running through django internal server

--- DjangoCloudCode/pinutcloud/dashboard/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class NameForm(forms.Form):
-    print "In form.py"
-    email = forms.EmailField(label='E-mail', max_length=100)
-    print "email",email
-    password = forms.CharField(label='Password', max_length=50)
-    print "password",password
